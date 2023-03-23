8a006161c26b86b26bd990f4ad3f8b9ab859ce35
CHO0178 <115779219+CHO0178@users.noreply.github.com>
2023-03-23 12:52:24 +0100

    recreate basic GPIO F and E

--- mkl25z4_template/src/demo_CHO0178/GPIO_basic_F.c
+++ mkl25z4_template/src/demo_CHO0178/GPIO_basic_F.c
@@ -1,5 +1,6 @@
 #include "MKL25Z4.h"
 #include "wdog.h"
+#include "littleHelper.h"
 
 /*
 
@@ -31,6 +32,8 @@ int main(void)
 {
 	wdog_init(WDOG_CONF_LPOCLK_1024_CYCLES);
 
+	cviceni1Setup();
+
 	while (1) {
 		wdog_refresh();
 	}
@@ -40,39 +43,53 @@ int main(void)
 
 void cviceni1Setup()
 {
-	// presmerujte signal z periferie GPIO na prislusny pin cervene diody
-
+	// presmerujte signal z periferie GPIO na pin příslušný pin cervene diody
+	PORTB->PCR[18] |= 1 << PORT_PCR_MUX_SHIFT;
 	// nastavte pin cervene led diody na output
-
+	GPIOB->PDDR |= 1 << 18;
 	// zapnete cervenou diodu
-
+	GPIOB->PDOR = 1 << 18;
 }
 
 void cviceni2Setup()
 {
 	// presmerujte signal z periferie GPIO na pin příslušný pin cervene a zelene diody
-
+	PORTB->PCR[18] |= 1 << PORT_PCR_MUX_SHIFT;
+	PORTB->PCR[19] |= PORT_PCR_MUX(1);
 	// nastavte cervenou a zelenou diodu na vystup
+	GPIOB->PDDR |= (1 << 18) | (1 << 19);
+
+
 
 }
 void cviceni2Loop()
 {
 	// blikejte stridave cervenou, zelenou a zadnou diodou
 	// zapni cervenou diodu
-
+	GPIOB->PDOR = 1 << 18;
 	// zamestnej procesor
-
+	for(int i = 0;i < 2000000; i++){}
 	// zapni zelenou diodu a vypni cervenou
-
+	GPIOB->PDOR = 1 << 19;
 	// zamestnej procesor
-
-	// vypni cervenou diodu
-
+	for(int i = 0;i < 2000000; i++){}
+	// vypni zeleou diodu
+	GPIOB->PDOR = 0;
 	// zamestnej procesor
-
+	for(int i = 0;i < 2000000; i++){}
 }
 
 void cviceni3Loop()
 {
 	// vytvor semafor:  R -> Y(red&green) -> G -> Y
+	GPIOB->PDOR = 1 << 18;
+	heavyFunction();
+	GPIOB->PDOR = (1 << 19) | (1 << 18);
+	heavyFunction();
+	GPIOB->PDOR = 1 << 19;
+	heavyFunction();
+	GPIOB->PDOR = (1 << 19) | (1 << 18);
+	heavyFunction();
+
 }
+
