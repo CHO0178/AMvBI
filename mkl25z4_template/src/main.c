a387eb3345920f42282fa90399ad7df991341138
CHO0178 <115779219+CHO0178@users.noreply.github.com>
2023-03-30 10:12:28 +0200

    original Main

--- mkl25z4_template/src/main.c
+++ mkl25z4_template/src/main.c
@@ -1,37 +1,19 @@
 /*
- * Name: gpio_pdir.c
- * Author: Martin Stankus
+ * Name: main.c
+ * Author: You
  *
- * Demo utilises BNC J9.
+ * This is a stub. Write your code here.
  *
  */
 
-#include "MKL25Z4.h"
-
-#include "soc_def.h"
-#include "res_alloc.h"
-
 #include "wdog.h"
 
-//do a ctrl-click on symbolic constants to see their meaning
-
-int main(void)
+__attribute__ ((weak)) int main(void)
 {
 	wdog_init(WDOG_CONF_LPOCLK_1024_CYCLES);
 
-	//the IOMASK_J9 bit of the GPIO_J9->PDDR register is already 0 (input)
-	PORT_J9->PCR[IOIND_J9] = PORT_PCR_MUX(PORT_PCR_MUX_VAL_GPIO);
-
-	GPIO_LEDG->PDDR |= IOMASK_LEDG;
-	PORT_LEDG->PCR[IOIND_LEDG] = PORT_PCR_MUX(PORT_PCR_MUX_VAL_GPIO);
-
+	int a;
 	while (1) {
-		if (GPIO_J9->PDIR & IOMASK_J9) {
-			GPIO_LEDG->PSOR = IOMASK_LEDG;
-		} else {
-			GPIO_LEDG->PCOR = IOMASK_LEDG;
-		}
-
 		wdog_refresh();
 	}
 
