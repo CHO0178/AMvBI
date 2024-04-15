bd008043b46c403658f124f2fb43fb742ff0abed
CHO0178 <115779219+CHO0178@users.noreply.github.com>
2024-04-15 10:33:13 +0200

    translate to english intrrupst_E

--- mkl25z4_template/src/demo_CHO0178/interrupt_E.c
+++ mkl25z4_template/src/demo_CHO0178/interrupt_E.c
@@ -10,20 +10,31 @@
 void blockingFunctionExample();
 void nonBlockingFunctionExample();
 
+// CZ: vyzkousejte co se stane pokud pouzijete blokovací a neblokovaci funkce v
+// ruznych kombinacich jako bezici funkci nebo preruseni a popiste jaky maji vliv
+// EN: try different combination of functions as base function and interrupt
+// function and describe what do you see
+
+
 int main(void)
 {
 	wdog_init(WDOG_CONF_LPOCLK_1024_CYCLES);
 	led_init();
 	btn_init();
 
-	// povol prijem preruseni pro port A a nastav prioritu na 2
+	// CZ: povol prijem preruseni pro port A a nastav prioritu na 2
+	// EN: enable processing of incoming interrupt signal from port A and set
+	// priority 2
 
-	// povol generovani preruseni v periferii port A pro sestupnou i n�stupnou hranu
-
+	// CZ: povol generovani preruseni v periferii port A pro sestupnou i
+	// nastupnou hranu
+	// EN: enable generating interrupt signal in port A for falling and rising
+	// edge
 
 	while (1) {
 		wdog_refresh();
-		// zavolej funkci
+		// CZ: zavolej funkci
+		// EN: call function
 
 	}
 
@@ -32,27 +43,36 @@ int main(void)
 
 void PORTA_IRQHandler(void)
 {
-	// vynuluj flag
+	// CZ: vynuluj flag
+	// EN: reset flag
 
-	// zavolej funkci
+	// CZ: zavolej funkci
+	// EN: call function
 
 }
 
 
 void blockingFunctionExample()
 {
+	//wait
 	for(int i = 0;i<2000000;i++){}
-	// zapni diodu 1 a vypni diodu 2
 
+	// CZ: zapni diodu 1 a vypni diodu 2
+	// EN: turn on diode 1 and turn off diode 2
+
+	//wait
 	for(int i = 0;i<2000000;i++){}
-	// zapni diodu 2 a vypni diodu 1
+
+	// CZ: zapni diodu 2 a vypni diodu 1
+	// EN: turn on diode 2 and turn off diode 1
 
 
 }
 
 void nonBlockingFunctionExample()
 {
-	// pokud je tlacitko 2 sepnute rozsvit diodu 3
+	// CZ: pokud je tlacitko 2 sepnute rozsvit diodu 3
+	// EN: if button 2 is pushed turn on diode 3
 
 }
 
