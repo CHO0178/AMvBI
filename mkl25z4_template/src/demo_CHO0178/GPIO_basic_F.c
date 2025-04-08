b9854129c4360a6de550d30e672f32340379af38
CHO0178 <115779219+CHO0178@users.noreply.github.com>
2025-04-08 08:29:25 +0200

    update GPIO basic F and hotfix little helper

--- mkl25z4_template/src/demo_CHO0178/GPIO_basic_F.c
+++ mkl25z4_template/src/demo_CHO0178/GPIO_basic_F.c
@@ -22,14 +22,27 @@ GPIO (CH41)
 	PDDR
 */
 
-void cviceni1Setup();
-void cviceni2Setup();
-void cviceni2Loop();
-void cviceni3Loop();
+/* CZ: cviceni:
+ * EN: exercizes:
+
+	setup: 1 loop: -
+	setup: 2 loop: -
+	setup: 2 loop: 2
+	setup: 2 loop: 3
+	setup: 4 loop: 4
+
+*/
+
+void exercise1Setup();
+void exercise2Setup();
+void exercise4Setup();
+void exercise2Loop();
+void exercise3Loop();
+void exercise4Loop();
 
 int main(void)
 {
-	wdog_init(WDOG_CONF_LPOCLK_1024_CYCLES);
+	wdog_init(WDOG_CONF_DIS);
 
 	cviceni1Setup();
 
@@ -40,47 +53,63 @@ int main(void)
 	return 0;
 }
 
-void cviceni1Setup()
+void exercise1Setup()
 {
-	// presmerujte signal z periferie GPIO na pin příslušný pin cervene diody
+	// CZ: presmerujte signal z periferie GPIO na prislusny pin cervene diody
+	// EN: redirect signal from GPIO peripherals to pin associated with red part
+	//	   of RGB diode
 	PORTB->PCR[18] |= 1 << PORT_PCR_MUX_SHIFT;
-	// nastavte pin cervene led diody na output
+	// CZ: nastavte pin cervene led diody na output
+	// EN: setup pin of associated with red diode as output
 	GPIOB->PDDR |= 1 << 18;
-	// zapnete cervenou diodu
+	// CZ: zapnete cervenou diodu
+	// EN: turn on red diode
 	GPIOB->PDOR &= ~(1 << 18);  // log. 0 0V dovoli protekat proudu skrze diodu viz datasheet
 }
 
-void cviceni2Setup()
+void exercise2Setup()
 {
 	// presmerujte signal z periferie GPIO na příslušný pin cervene a zelene diody
+	// EN: redirect signal from GPIO peripherals to pin associated with red and
+	//	   green part of RGB diode
 	PORTB->PCR[18] |= 1 << PORT_PCR_MUX_SHIFT;
 	PORTB->PCR[19] |= PORT_PCR_MUX(1);
 	// nastavte cervenou a zelenou diodu na vystup
+	// GPIOB->PDDR = (1<<18) | (1<<19);
 	GPIOB->PDDR |= (1 << 18) | (1 << 19);
 
 
 
 }
-void cviceni2Loop()
+void exercise2Loop()
 {
-	// blikejte stridave cervenou, zelenou a zadnou diodou
-	// zapni cervenou diodu
+	// CZ: blikejte stridave cervenou, zelenou a zadnou diodou
+	// EN: bling alternately red, green and none diode
+	// *******************************
+	// CZ: zapni cervenou diodu
+	// EN: turn on red diode
 	GPIOB->PDOR = 1 << 19;   //nastaveni zelene na log. 1 (3,3V) zamezi proudu diodou
-	// zamestnej procesor
+	// CZ: zamestnej procesor
+	// EN: utilize processor
 	for(int i = 0;i < 2000000; i++){}
-	// zapni zelenou diodu a vypni cervenou
+	// CZ: zapni zelenou diodu a vypni cervenou
+	// EN: turn on greed diode and turn off red diode
 	GPIOB->PDOR = 1 << 18;
-	// zamestnej procesor
+	// CZ: zamestnej procesor
+	// EN: utilize processor
 	for(int i = 0;i < 2000000; i++){}
-	// vypni zeleou diodu
+	// CZ: vypni zelenou diodu
+	// EN: turn off green diode
 	GPIOB->PDOR = 0;
-	// zamestnej procesor
+	// CZ: zamestnej procesor
+	// EN: utilize processor
 	for(int i = 0;i < 2000000; i++){}
 }
 
-void cviceni3Loop()
+void exercise3Loop()
 {
-	// vytvor semafor:  R -> Y(red&green) -> G -> Y
+	// CZ: vytvor semafor:  R -> Y(red&green) -> G -> Y
+		// EN: create semaphor: R -> Y(red&green) -> G -> Y
 	GPIOB->PDOR |= 1 << 19;
 	heavyFunction();
 	GPIOB->PDOR = 0;
@@ -91,14 +120,16 @@ void cviceni3Loop()
 	heavyFunction();
 }
 
-void cviceni4Setup()
+void exercise4Setup()
 {
-	//presmerujte signal z periferie GPIO na příslušný pin tlacitka
+	// CZ: presmerujte signal z periferie GPIO na příslušný pin tlacitka
+	// EN: redirect signal from GPIO peripherals to pin associated with button 1
 	PORTA->PCR[4] = 1<<PORT_PCR_MUX_SHIFT;
 }
 
-void cviceni4Loop()
+void exercise4Loop()
 {
-	//vlozte hodnotu z tlacitka na diodu
+	// CZ: vlozte hodnotu z tlacitka na diodu
+	// EN: move value of button input to diode output
 	GPIOB->PDOR = (GPIOA->PDIR>>4)<<18;
 }
--- mkl25z4_template/src/demo_CHO0178/littleHelper.h
+++ mkl25z4_template/src/demo_CHO0178/littleHelper.h
@@ -8,7 +8,7 @@
 #define toogleBitInRegister(REG, pos)  						REG ^= 1 << pos
 #define changeMultipleBitInRegister(REG, DATA, MASK, pos)   REG = (DATA << pos) | (REG & (~(MASK & REG)))
 #define setMultipleBitsInRegister(REG,DATA,MASK)			REG = (DATA & (~MASK)) | (DATA & MASK)
-//void heavyFunction() {for(int i = 0;i < 2000000; i++){}}
+void heavyFunction() {for(int i = 0;i < 2000000; i++){}}
 
 
 
