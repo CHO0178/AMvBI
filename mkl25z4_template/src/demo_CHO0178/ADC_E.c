f8c9ca72417e2412b4861fc6a6774cec954dcb62
CHO0178 <115779219+CHO0178@users.noreply.github.com>
2025-04-29 15:52:29 +0200

    new version ADC lesson

--- mkl25z4_template/src/demo_CHO0178/ADC_E.c
+++ mkl25z4_template/src/demo_CHO0178/ADC_E.c
@@ -16,7 +16,7 @@ ADC
 		COCO	// complete conversion flag
 		DIFF	// differential mode of measurement
 	SC3
-		ADC0	// is continuous on one sample
+		ADCO	// is continuous on one sample
 	R			// result
 
 PORT
@@ -30,28 +30,22 @@ PORT
 
 #include "led.h"
 
-void startContinuousMeasuring(uint32_t channel);
+void exercise1();
+void exercise2();
+void exercise3();
+void startContinuousMeasuring(int pot);
 void setSingleTimeMeasurementOnTrigger();
 int blockingMeasurementOfPotentiometer(int pot);
-void nonBlockingSetOfLedBarGraphToValueFromADC();
+int nonBlockingGetADCval();
 
 
 __attribute__ ((weak)) int main(void)
 {
 	wdog_init(WDOG_CONF_DIS);
 	led_init();
-	// CZ: zavolejte funkci pro nastaveni ADC
-	// EN: call initialize ADC
-	//startContinuousMeasuring(0b1100); // 0b1011  0b1100
-	//setSingleTimeMeasurementOnTrigger();
-
-
-	while (1) {
-		// CZ: ziskejte hodnotu z potenciometru a nastavte bargraf
-		// EN: get value from potentiometer and set bargraph to this value
-		//blockingMeasurementOfPotentiometer(1); //2
-		//nonBlockingSetOfLedBarGraphToValueFromADC();
-	}
+	// CZ: zavolejte cviceni
+	// EN: call exercize
+	exercise1();
 	return 0;
 }
 
@@ -61,7 +55,7 @@ __attribute__ ((weak)) int main(void)
  * @param channel value "ADC channel" of channel connected to
  * potentiometer (refMan pg. 79)
  */
-void startContinuousMeasuring(uint32_t channel)
+void startContinuousMeasuring(int pot)
 {
 	// CZ: nastavte delicku hodinoveho signalu na 4 a 8-bitovou konverzi
 	// EN: set divider of signal source to divide by 4 and 8-bit conversion
@@ -73,6 +67,7 @@ void startContinuousMeasuring(uint32_t channel)
 
 	// CZ: trigrujte ADC kolnverzi zmennou kanalu
 	// EN: trigger ADC conversion at channel
+
 
 }
 
@@ -99,44 +94,95 @@ void setSingleTimeMeasurementOnTrigger()
  */
 int blockingMeasurementOfPotentiometer(int pot)
 {
+	int result;
 	// CZ: spustte mereni hodnoty na potenciometru
 	// EN: trigger measuring of potentiometer voltage
-	switch(pot)
-	{
-		case 1:
-			// CZ: trigrujte kanal potenciometru 1
-			// EN: trigger channel of potentiometer 1
 
-			break;
-		case 2:
-			// CZ: trigrujte kanal potenciometru 2
-			// EN: trigger channel of potentiometer 2
-
-			break;
-		default:
-			// CZ: vratte chybovou hodnotu
-			// EN: return error not existing potentiometer
-			return -1;
-			break;
-	}
 	// CZ: vyckejte na dokonceni konverze a zvednuti priznaku preruseni
 	// EN: wait for complete conversion flag to rise to rise
 
 
-	return 0;
+
+	return result;
 }
 
 /**
- * @brief measure value of potentiometer and set diodes according this value
+ * @brief capture and return value of analog signal
  */
-void nonBlockingSetOfLedBarGraphToValueFromADC()
+int nonBlockingGetADCval()
 {
 	// CZ: pote co je dokoncen prevod analogu na digital
-	// nastavte bargraf podle prelozene hodnoty
-	// EN: if conversion is complete set the bar graph
-	// regarding received value
+	// nastavte vratte jeho hodnotu jinak -1
+	// EN: if conversion is complete return result else -1
 
+
+	return -1;
 }
 
+/**
+ * @brief
+ * 	CZ: vyuzije mereni s trigrovanim pred kazdym prevodem a vykreslete
+ * 	hodnotu potenciometru na bargraf vzdy po dokonceni prevodu
+ * 	EN: trigger ADC before every measurement and plot result after
+ * 	converison is completed
+ */
+void exercise1(){
+	int val;
+	// CZ: zavolejte funkci pro nastaveni ADC
+	// EN: call initialize ADC
+	setSingleTimeMeasurementOnTrigger();
+
+	while (1) {
+		// CZ: ziskejte hodnotu z potenciometru a nastavte bargraf
+		// EN: get value from potentiometer and set bargraph to this value
+		val = blockingMeasurementOfPotentiometer(1);
+		led_bindisp(val);
+	}
+}
+
+/**
+ * @brief
+ * 	CZ: vyuzije kontunualni mereni a vykresluje
+ * hodnotu potenciometru na bargraf vzdy po dokonceni prevodu
+ * 	EN: using continuous measurement and displaying the status
+ * of the potentiometer on the bargraph immediately after the
+ * conversion is completed.
+ */
+void exercise2(){
+	int val;
+
+	// CZ: zavolejte funkci pro nastaveni ADC
+	// EN: call initialize ADC
+	startContinuousMeasuring(2);
+	while (1) {
+		// CZ: pokud je navracena hodnota vetsi nebo rovno 0 vykresli na bargraf
+		// EN: get value from potentiometer and set bargraph to this value
+		val = nonBlockingGetADCval();
+		if(val>=0)
+		{
+			led_bindisp(val);
+		}
+	}
+}
+
+/**
+ * @brief
+ * 	CZ: funkce secte hodnoty obou potenciometru a vykresli na bargraf.
+ * 	maximalni hodnota je soucet obou maxim potenciometru
+ * 	EN: function add values of both potentiometers and show result on
+ * 	bargraph sum of both value is maximum of both potentiometers maxims
+ */
+void exercise3(){
+
+	// CZ: zavolejte funkci pro nastaveni ADC
+	// EN: call initialize ADC
+
+
+	while (1) {
+		// CZ: ziskejte hodnotu z potenciometru a nastavte bargraf
+		// EN: get value from potentiometer and set bargraph to this value
+
+	}
+}
 
 
--- mkl25z4_template/src/demo_CHO0178/ADC_F.c
+++ mkl25z4_template/src/demo_CHO0178/ADC_F.c
@@ -7,16 +7,16 @@
  * @version 1
  */
 /*
-
-ADC(ch.28)
+ADC
 	CFG1
 		ADIV	// clk signal divider
 		MODE	// N-bit translation
 	SC1
-		ADCH	// channel select
+		ADCH	// channel select (trigger)
 		COCO	// complete conversion flag
+		DIFF	// differential mode of measurement
 	SC3
-		ADC0	// is continuous on one sample
+		ADCO	// is continuous on one sample
 	R			// result
 
 PORT
@@ -30,53 +30,64 @@ PORT
 
 #include "led.h"
 
-void startContinuousMeasuring(uint32_t channel);
+void exercise1();
+void exercise2();
+void exercise3();
+void startContinuousMeasuring(int pot);
 void setSingleTimeMeasurementOnTrigger();
 int blockingMeasurementOfPotentiometer(int pot);
-void nonBlockingSetOfLedBarGraphToValueFromADC();
+int nonBlockingGetADCval();
 
 
 __attribute__ ((weak)) int main(void)
 {
 	wdog_init(WDOG_CONF_DIS);
 	led_init();
-	// CZ: nastavte ADC
-	// EN: initialize ADC
-
-	startContinuousMeasuring(0b1100); // 0b1011  0b1100
-	setSingleTimeMeasurementOnTrigger();
-
-
-	while (1) {
-		// CZ: ziskejte hodnotu z potenciometru a nastavte bargraf
-		// EN: get value from potentiometer and set bargraph to this value
-		int val1 = blockingMeasurementOfPotentiometer(1);
-		int val2 = blockingMeasurementOfPotentiometer(2);
-		led_bgraph(((val1+val2)/2)* 0.035f);
-		//nonBlockingSetOfLedBarGraphToValueFromADC();
-	}
+	// CZ: zavolejte cviceni
+	// EN: call exercize
+	exercise3();
 	return 0;
 }
 
 
 /**
  * @brief set ADC to continuous measuring and start measurement
- * @param channel value of channel connected to
- * potentiometer from refMan pg. 79
+ * @param channel value "ADC channel" of channel connected to
+ * potentiometer (refMan pg. 79)
  */
-void startContinuousMeasuring(uint32_t channel)
+void startContinuousMeasuring(int pot)
 {
-	// CZ: nastavte delicku hodinoveho signalu na 4 a 9-bitovou konverzi
-	// EN: set divider of signal source to divide by 4 and 9-bit conversion
-	ADC0->CFG1 = 0b10 << ADC_CFG1_ADIV_SHIFT;
+	// CZ: nastavte delicku hodinoveho signalu na 4 a 8-bitovou konverzi
+	// EN: set divider of signal source to divide by 4 and 8-bit conversion
+	ADC0->CFG1 |= 0b10 << ADC_CFG1_ADIV_SHIFT;
 
 	// CZ: nastavte ADC na kontinualni mereni
 	// EN: set ADC continuous measurement
-	ADC0->SC3 = ADC_SC3_ADCO_MASK;
+	ADC0->SC3 |= ADC_SC3_ADCO_MASK;
 
-	// CZ: trigrujte ADC kolnverzi na cahnnel
+	// CZ: trigrujte ADC kolnverzi zmennou kanalu
 	// EN: trigger ADC conversion at channel
-	ADC0->SC1[0] = channel;
+	switch(pot)
+	{
+		case 1:
+			// CZ: trigrujte kanal potenciometru 1
+			// EN: trigger channel of potentiometer 1
+			ADC0->SC1[0] &= ~(0b11111);
+			ADC0->SC1[0] |= 0b01011;
+			break;
+		case 2:
+			// CZ: trigrujte kanal potenciometru 2
+			// EN: trigger channel of potentiometer 2
+			ADC0->SC1[0] &= ~(0b11111);
+			ADC0->SC1[0] |= 0b01100;
+			break;
+		default:
+			// CZ: vratte chybovou hodnotu
+			// EN: return error not existing potentiometer
+			return;
+			break;
+	}
+
 }
 
 
@@ -85,11 +96,11 @@ void startContinuousMeasuring(uint32_t channel)
  */
 void setSingleTimeMeasurementOnTrigger()
 {
-	// CZ: nastavte delicku hodinoveho signalu na 4 a 9-bitovou konverzi
-	// EN: set divider of signal source to divide by 4 and 9-bit conversion
-	ADC0->CFG1 = 0b10 << ADC_CFG1_ADIV_SHIFT;
+	// CZ: nastavte delicku hodinoveho signalu na 4 a 8-bitovou konverzi
+	// EN: set divider of signal source to divide by 4 and 8-bit conversion
+	ADC0->CFG1 |= (0b10 << 5);
 
-	// CZ: nastavte
+	// CZ: nastavte mereni na kontinnualni
 	// EN: set measurement to continuous measurement
 	ADC0->SC3 &= ~ADC_SC3_ADCO_MASK;
 }
@@ -102,6 +113,7 @@ void setSingleTimeMeasurementOnTrigger()
  */
 int blockingMeasurementOfPotentiometer(int pot)
 {
+	int result;
 	// CZ: spustte mereni hodnoty na potenciometru
 	// EN: trigger measuring of potentiometer voltage
 	switch(pot)
@@ -109,12 +121,14 @@ int blockingMeasurementOfPotentiometer(int pot)
 		case 1:
 			// CZ: trigrujte kanal potenciometru 1
 			// EN: trigger channel of potentiometer 1
-			ADC0->SC1[0] = 0B1011;
+			ADC0->SC1[0] &= ~(0b11111);
+			ADC0->SC1[0] |= 0b01011;
 			break;
 		case 2:
 			// CZ: trigrujte kanal potenciometru 2
 			// EN: trigger channel of potentiometer 2
-			ADC0->SC1[0] = 0b1100;
+			ADC0->SC1[0] &= ~(0b11111);
+			ADC0->SC1[0] |= 0b01100;
 			break;
 		default:
 			// CZ: vratte chybovou hodnotu
@@ -122,21 +136,98 @@ int blockingMeasurementOfPotentiometer(int pot)
 			return -1;
 			break;
 	}
-	// CZ: vyckejte na dokonceni konverze a zvednuti priznaku
+	// CZ: vyckejte na dokonceni konverze a zvednuti priznaku preruseni
 	// EN: wait for complete conversion flag to rise to rise
-	while(!(ADC0->SC1[0] & ADC_SC1_COCO_MASK)){}
+	while(!(ADC0->SC1[0] & (1<<7))){}  //waiting for flag
+	result = ADC0->R[0];
 
-	return ADC0->R[0];
+	return result;
 }
 
-void nonBlockingSetOfLedBarGraphToValueFromADC()
+/**
+ * @brief capture and return value of analog signal
+ */
+int nonBlockingGetADCval()
 {
 	// CZ: pote co je dokoncen prevod analogu na digital
-	// nastavte bargraf podle prellozene hodnoty
-	// EN: if conversion is complete set the bar graph
-	// regarding received value
-	if(ADC0->SC1[0] & ADC_SC1_COCO_MASK)
+	// nastavte vratte jeho hodnotu jinak -1
+	// EN: if conversion is complete return result else -1
+	if(ADC0->SC1[0] & (1<<7))
 	{
-		led_bgraph(ADC0->R[0u] * 0.035f);  //led_bgraph 0-8 diode
+		return ADC0->R[0];
+	}
+	else{
+		return -1;
 	}
 }
+
+/**
+ * @brief
+ * 	CZ: vyuzije mereni s trigrovanim pred kazdym prevodem a vykreslete
+ * 	hodnotu potenciometru na bargraf vzdy po dokonceni prevodu
+ * 	EN: trigger ADC before every measurement and plot result after
+ * 	converison is completed
+ */
+void exercise1(){
+	int val;
+	// CZ: zavolejte funkci pro nastaveni ADC
+	// EN: call initialize ADC
+	setSingleTimeMeasurementOnTrigger();
+
+	while (1) {
+		// CZ: ziskejte hodnotu z potenciometru a nastavte bargraf
+		// EN: get value from potentiometer and set bargraph to this value
+		val = blockingMeasurementOfPotentiometer(1);
+		led_bindisp(val);
+	}
+}
+
+/**
+ * @brief
+ * 	CZ: vyuzije kontunualni mereni a vykresluje
+ * hodnotu potenciometru na bargraf vzdy po dokonceni prevodu
+ * 	EN: using continuous measurement and displaying the status
+ * of the potentiometer on the bargraph immediately after the
+ * conversion is completed.
+ */
+void exercise2(){
+	int val;
+
+	// CZ: zavolejte funkci pro nastaveni ADC
+	// EN: call initialize ADC
+	startContinuousMeasuring(2);
+	while (1) {
+		// CZ: pokud je navracena hodnota vetsi nebo rovno 0 vykresli na bargraf
+		// EN: get value from potentiometer and set bargraph to this value
+		val = nonBlockingGetADCval();
+		if(val>=0)
+		{
+			led_bindisp(val);
+		}
+	}
+}
+
+/**
+ * @brief
+ * 	CZ: funkce secte hodnoty obou potenciometru a vykresli na bargraf.
+ * 	maximalni hodnota je soucet obou maxim potenciometru
+ * 	EN: function add values of both potentiometers and show result on
+ * 	bargraph sum of both value is maximum of both potentiometers maxims
+ */
+void exercise3(){
+	int val1;
+	int val2;
+	// CZ: zavolejte funkci pro nastaveni ADC
+	// EN: call initialize ADC
+	setSingleTimeMeasurementOnTrigger();
+
+	while (1) {
+		// CZ: ziskejte hodnotu z potenciometru a nastavte bargraf
+		// EN: get value from potentiometer and set bargraph to this value
+		val1 = blockingMeasurementOfPotentiometer(1);
+		val2 = blockingMeasurementOfPotentiometer(2);
+		led_bindisp((val1+val2)/2);
+	}
+}
+
+
